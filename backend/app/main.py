6239acdaf864797f4dde1ae1fe1fd704a3a67e60
bhuwadatharva <bhuwad.atharva@gmail.com>
2026-07-16 17:57:45 +0530

    Add production URL to CORS middleware configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://aivoa-1-thvw.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
